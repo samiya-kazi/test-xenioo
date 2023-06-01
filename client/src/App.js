bc237454288bf93c2119af33bbc0c03958553dcd
Samiya Kazi <samiya.kazi09@gmail.com>
2023-06-01 17:10:06 +0600

    removed script from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,10 +20,6 @@ function App() {
 
   return (
     <div className="App">
-      <script data-id='xenioo' data-node='cm91dGluZy54ZW5pb28uY29t' src="https://res.xenioo.com/plugin/xenioo.js"></script>
-      <script>
-        xenioowebchat.Start("iYFxmYaiB5zTfpdBjPbNSiEJLKVk58F5plNyiqzH9gjy46Jw23QihKMzJwbS3qc2");
-      </script>
     </div>
   );
 }

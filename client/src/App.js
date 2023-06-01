ce066aadf0cc621aee9194fe5149ebe99d8da646
Samiya Kazi <samiya.kazi09@gmail.com>
2023-06-01 16:47:25 +0600

    added xenioo chatbot

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <script data-id='xenioo' data-node='cm91dGluZy54ZW5pb28uY29t' src="https://res.xenioo.com/plugin/xenioo.js"></script>
+      <script>
+        xenioowebchat.Start("iYFxmYaiB5zTfpdBjPbNSiEJLKVk58F5plNyiqzH9gjy46Jw23QihKMzJwbS3qc2");
+      </script>
     </div>
   );
 }
